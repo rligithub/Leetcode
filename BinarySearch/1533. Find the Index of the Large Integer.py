4e75ea1b45a5bd3cac8b835f9b436f9ace45c09b
Rika <rikanyl@gmail.com>
2022-05-16 22:36:34 -0700

    all

--- BinarySearch/1533. Find the Index of the Large Integer.py
+++ BinarySearch/1533. Find the Index of the Large Integer.py
@@ -2,16 +2,17 @@
 # This is ArrayReader's API interface.
 # You should not implement it, or speculate about its implementation
 # """
-# class ArrayReader(object):
-#	 # Compares the sum of arr[l..r] with the sum of arr[x..y]
-#	 # return 1 if sum(arr[l..r]) > sum(arr[x..y])
-#	 # return 0 if sum(arr[l..r]) == sum(arr[x..y])
-#	 # return -1 if sum(arr[l..r]) < sum(arr[x..y])
-#    def compareSub(self, l: int, r: int, x: int, y: int) -> int:
-#
-#	 # Returns the length of the array
-#    def length(self) -> int:
-#
+class ArrayReader(object):
+	 # Compares the sum of arr[l..r] with the sum of arr[x..y]
+	 # return 1 if sum(arr[l..r]) > sum(arr[x..y])
+	 # return 0 if sum(arr[l..r]) == sum(arr[x..y])
+	 # return -1 if sum(arr[l..r]) < sum(arr[x..y])
+   def compareSub(self, l: int, r: int, x: int, y: int) -> int:
+       pass
+
+       # Returns the length of the array
+   def length(self) -> int:
+       pass
 
 
 class Solution:
@@ -24,7 +25,7 @@ class Solution:
                 read = reader.compareSub(left, mid, mid + 1, right)  # even
             else:
                 read = reader.compareSub(left, mid - 1, mid + 1, right)  # odd
-            if read == 0
+            if read == 0:
                 return mid
             elif read == 1:
                 right = mid
